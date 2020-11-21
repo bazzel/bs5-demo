52e499c4ba49cc5761907dc0299c52b0110588a3
Patrick Baselier <patrick.baselier@gmail.com>
2020-11-21 10:39:13 +0100

    Install bs5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+helper Bs5::Engine.helpers
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Bs5::Engine => '/bs5'
+  get '/', to: redirect('/bs5')
 end
